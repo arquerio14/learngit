91ef7263c0488e51af286fa81fe4169b70513d36
Muhammed Mustafa Gökçeoğlu <mgokceoglu09@gmail.com>
2024-03-05 16:36:53 +0300

    daily update 'studies and projects'

---
+++ IdeaProjects/methodOverloading/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        System.out.println(dortIslem.topla(4,7,2));
+    }
+}
